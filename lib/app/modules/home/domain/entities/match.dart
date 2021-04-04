3903f3b19d7acd81f4ae1e7b4d86ebc0a2d503af
kelveng.magalhaes <kelveng.magalhaes@tupiconsultoria.com.br>
2021-04-04 12:31:25 -0300

    Match tem uma lista de Games

--- lib/app/modules/home/domain/entities/match.dart
+++ lib/app/modules/home/domain/entities/match.dart
@@ -1,9 +1,16 @@
+import 'game.dart';
 import 'status_match.dart';
 
 class Match {
   final int id;
   final String description;
   final StatusMatch status;
+  final List<Game> games;
 
-  Match(this.id, this.description, {this.status = StatusMatch.none});
+  Match(
+    this.id,
+    this.description,
+    this.status,
+    this.games,
+  );
 }
